4aede0c79f19ff427e3f435461710fc3260072bd
SpideryBook78 <ch374185@gmail.com>
2025-05-21 21:33:07 -0600

    Routes with link and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/layout/Header';
+import Home from './components/views/Home';
+import About from './components/views/About';
+import Contact from './components/views/Contact';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1 className="font-extrabold text-2xl">Black´s Shadows Site</h1>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </Router>
   );
 };
 
 export default App;
-
