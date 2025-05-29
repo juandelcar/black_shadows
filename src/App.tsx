5c855c6e346f4a8c612260997ac332e4641dc56d
SpideryBook78 <ch374185@gmail.com>
2025-05-29 13:53:24 -0600

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
 import Home from './components/views/Home';
 import Contact from './components/views/Contact';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
